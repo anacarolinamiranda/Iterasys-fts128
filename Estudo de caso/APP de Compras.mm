4093029dc383d56a40f19c2b65cc4bdb798fb8f8
anacarolinamiranda <73615219+anacarolinamiranda@users.noreply.github.com>
2020-10-28 21:03:35 -0300

    Commit Inicial
    Criada pasta Estudo de Caso, e dentro dela arquivos do aplicativo.
    Relativos ao planejamento do software ficticio de listas de compras

---
+++ Estudo de caso/APP de Compras.mm
@@ -0,0 +1,25 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1603672281445" ID="ID_213409350" MODIFIED="1603673102899" TEXT="APP de Compras">
+<icon BUILTIN="list"/>
+<node CREATED="1603672383195" FOLDED="true" HGAP="37" ID="ID_1619890427" MODIFIED="1603673138255" POSITION="left" TEXT="Objetivos" VSHIFT="-8">
+<icon BUILTIN="yes"/>
+<node CREATED="1603672424924" HGAP="10" ID="ID_133963047" MODIFIED="1603672650964" TEXT="Consulta e compara&#xe7;&#xe3;o de valores.  Uma maneira de aproveitar as melhores ofertas de forma simples. " VSHIFT="50">
+<font NAME="SansSerif" SIZE="11"/>
+</node>
+</node>
+<node CREATED="1603672737780" FOLDED="true" ID="ID_6719949" MODIFIED="1603673119745" POSITION="left" TEXT="Publico Alvo">
+<icon BUILTIN="female1"/>
+<node CREATED="1603672755237" ID="ID_1647583261" MODIFIED="1603673083744" TEXT="Projeto Baseado em Persona de 54 anos, app o mais simples poss&#xed;vel para facilitar as buscas, devido a golpes e Fake News.">
+<font NAME="SansSerif" SIZE="11"/>
+</node>
+</node>
+<node CREATED="1603672817304" FOLDED="true" ID="ID_1163930327" MODIFIED="1603673187220" POSITION="left" TEXT="Telas ">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1603672850899" ID="ID_1235987925" MODIFIED="1603672873851" TEXT="Digitar o nome do produto e Buscar"/>
+<node CREATED="1603672874517" ID="ID_1207519360" MODIFIED="1603672992061" TEXT="Resultado da Busca, com as informa&#xe7;&#xf5;es sobre valores lado a lado de lojas diferentes. Ao lado uma lista com os nomes das lojas e o link do site. Bot&#xe3;o para filtrar a busca. Bot&#xe3;o para Ajuda.">
+<font NAME="SansSerif" SIZE="11"/>
+</node>
+</node>
+</node>
+</map>
